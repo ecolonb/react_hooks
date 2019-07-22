94769371207525ad5791776e3aae4addd2e748b4
Edd Colbar <edgar.colonb@gmail.com>
2019-07-21 21:50:30 -0500

    Agregando css_grid page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import RegisterPage from '../../pages/register/RegisterPage';
-
+import CssGrid from '../../pages/css_grid/CssGrid';
 function App() {
   return (
-    <div className="App">
-      <RegisterPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/" component={CssGrid} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/css_grid/CssGrid.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './cssGrid.css';
+export default () => {
+  return (
+    <div className="grid-container">
+      <div className="item">Contenido #1</div>
+      <div className="item">Contenido #2</div>
+      <div className="item">Contenido #3</div>
+      <div className="item">Contenido #4</div>
+      <div className="item">Contenido #5</div>
+      <div className="item">Contenido #6</div>
+      <div className="item">Contenido #7</div>
+      <div className="item">Contenido #8</div>
+      <div className="item">Contenido #9</div>
+      <div className="item">Contenido #10</div>
+      <div className="item">Contenido #11</div>
+      <div className="item">Contenido #12</div>
+    </div>
+  );
+};
